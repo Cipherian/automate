944948e42d86be764295309e3b0c0be578c0c480
Daniel Brott <danbrott42@gmail.com>
2023-03-28 23:18:01 -0700

    more testing with python automation

--- shelve_module/shelve_practice.py
+++ shelve_module/shelve_practice.py
@@ -1,7 +1,9 @@
 import shelve
 import shutil
 import os.path
-
+import os
+import glob
+from pathlib import Path
 
 data = {"name": "John", "age": 30, "email": "john@example.com"}
 data2 = {"name": "Jane", "age": 25, "email": "jane@example.com"}
@@ -19,4 +21,17 @@ new_file_path = os.path.join(script_dir, "shelve_quiz.txt")
 with open(new_file_path, "w") as f:
     f.write(str(data))
 
-shutil.copyfile("data.db", os.path.join(script_dir, "data2.db"))
+# shutil.copyfile("data.db", os.path.join(script_dir, "data2.db")) # copies file
+
+files = os.listdir(script_dir)
+
+files_2 = glob.glob(os.path.join(script_dir, "*.txt")) # finds all txt files in the directory and prints the route
+
+p = Path(".")
+
+if __name__ == "__main__":
+    print(files) # shelve_quiz.txt, shelve_practice.py
+    print(files_2) # automate.py/shelve_module/shelve_quiz.txt'
+    for file in p.iterdir():
+        if file.is_file():
+            print(file.name)
